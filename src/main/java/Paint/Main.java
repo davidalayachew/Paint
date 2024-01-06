8e54e8968691034a1826a99159f85a34bc98df3d
davidalayachew <davidalayachew@gmail.com>
2024-01-05 23:39:18 -0500

    Removing ZoomableImage. Also, committing before SO

--- src/main/java/Paint/Main.java
+++ src/main/java/Paint/Main.java
@@ -1,14 +1,15 @@
 
 package Paint;
 
-import java.awt.Color;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.awt.*;
+import java.awt.image.*;
+import javax.swing.*;
+import java.util.*;
 
 public class Main
 {
+
+   private static final int SIZE = 1000;
 
    public static void main(String[] args)
    {
